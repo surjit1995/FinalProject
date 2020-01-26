bfa8840e0329b4bae20accc0deb05372828ecb48
surjit1995 <56876486+surjit1995@users.noreply.github.com>
2020-01-26 18:36:04 -0500

    Add files via upload

---
+++ FinaliOSProject/Resources/Constant.swift
@@ -0,0 +1,8 @@
+
+import Foundation
+import UIKit
+
+class Constants{
+    static var primaryColor = UIColor(displayP3Red: 36/255, green: 38/255, blue: 37/255, alpha: 1)
+
+}
---
+++ FinaliOSProject/Resources/Functions.swift
@@ -0,0 +1,21 @@
+
+
+
+import Foundation
+
+let applicationDocumentsDirectory: URL = {
+  let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+  return paths[0]
+}()
+
+let CoreDataSaveFailedNotification = Notification.Name(rawValue: "CoreDataSaveFailedNotification")
+
+func afterDelay(_ seconds: Double, run: @escaping () -> Void) {
+  DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: run)
+}
+
+func fatalCoreDataError(_ error: Error) {
+  print("*** Fatal error: \(error)")
+  NotificationCenter.default.post(name: CoreDataSaveFailedNotification, object: nil)
+}
+
